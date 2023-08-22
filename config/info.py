7cdbee9ffe2ca82c12f60a34f20bf1a8bdd8b97a
yongyeon <wlsdyddus1@naver.com>
2023-08-22 17:13:06 +0900

    first commit

--- config/info.py
+++ config/info.py
@@ -2,17 +2,16 @@ from array import *
 
 
 class InFo:
-    devices = "R3CR50AG9MH"
-    name = "진용연"   # 홍길동
-    rrn_a = "930709"  # 880101
-    rrn_b = "1" #주민번호 뒤에 첫자리 1,2,3,4
-    rrn = "017222" #주민번호 뒤에 첫자리를 제외한 6자리
-    rrnfull = "9307091017222" #주민번호 13자리
-    phone_number = "01042434533" #전화번호 - 제외 하고 01000000000
-    news_agency = "KT" # 통신사 정보 (KT,LGU+,SKT,KT알뜰폰,등등)
+    devices = ""
+    name = ""   # 홍길동
+    rrn_a = ""  # 880101
+    rrn_b = "" #주민번호 뒤에 첫자리 1,2,3,4
+    rrn = "" #주민번호 뒤에 첫자리를 제외한 6자리
+    rrnfull = "" #주민번호 13자리
+    phone_number = "" #전화번호 - 제외 하고 01000000000
+    news_agency = "" # 통신사 정보 (KT,LGU+,SKT,KT알뜰폰,등등)
     user_id = []
     usertoken = []
-
     txseqno = []
     idtoken = []
     # day = "20230725"  #오늘날짜
