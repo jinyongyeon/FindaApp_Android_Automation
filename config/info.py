cb088c7ffa29559ee8738958759b683d34b3f7aa
yongyeon <wlsdyddus1@naver.com>
2024-01-24 16:30:27 +0900

    first commit

--- config/info.py
+++ config/info.py
@@ -1,14 +1,14 @@
 class InFo:
     # 필수 입력 값
-    devices = "R3CR50AG9MH" # 디바이스 id 입력
-    name = "진용연"   # 홍길동
-    rrn_a = "930709"  # 880101
-    rrn_b = "1" # 주민번호 뒤에 첫자리 1,2,3,4 등
-    loan_rrn = "1017222" # 주민번호 뒤에 7자리
-    autoloan_rrn = "017222" # 주민번호 뒤에 첫자리를 제외한 6자리
-    rrnfull = "9307091017222" #주민번호 13자리
-    phone_number = "01042434533" #전화번호 - 제외 하고 01000000000
-    news_agency = "KT" # 통신사 정보 (KT,LGU+,SKT,KT알뜰폰,등등)
+    devices = "" # 디바이스 id 입력
+    name = ""   # 홍길동
+    rrn_a = ""  # 880101
+    rrn_b = "" # 주민번호 뒤에 첫자리 1,2,3,4 등
+    loan_rrn = "" # 주민번호 뒤에 7자리
+    autoloan_rrn = "" # 주민번호 뒤에 첫자리를 제외한 6자리
+    rrnfull = "" #주민번호 13자리
+    phone_number = "" #전화번호 - 제외 하고 01000000000
+    news_agency = "" # 통신사 정보 (KT,LGU+,SKT,KT알뜰폰,등등)
 
     # 미입력 영역
     user_id = []
